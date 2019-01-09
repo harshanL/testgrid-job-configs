e1d6122b3d21f3c3275a021aab4a9dab4558e76e
Harshan Liyanage <harshan@wso2.com>
2019-01-09 16:35:39 +0530

    Replace params.pp variables

---
+++ replace-parameters.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+strindex() {
+  x="${1%%$2*}"
+  [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+
+for param in "$@"
+do
+    idx=$(strindex $param =)
+    paramName=${param:0:$idx}
+    paramValue=${param:$idx+1}
+    sed -i "s|${paramName}.*|${paramName} = '${paramValue}'|g" params.pp
+done
